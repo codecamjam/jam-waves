87e6442d3b189d20319b907d18588b68a7f5eca9
codecamjam <camerontaghlabi@gmail.com>
2023-12-11 21:15:06 -0500

    fix prevent resize on animation

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,4 @@
 export { getAppHeight } from "./getAppHeight";
 export { shows } from "./shows-text";
 export { toggleDarkMode } from "./toggleDarkMode";
+export { preventResizeAnime } from "./preventResizeAnime";
